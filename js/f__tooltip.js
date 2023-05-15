908dd46748c66e18f040e0d894b0b2abb9ce7054
ladasedlacek <sedlacek.lada@gmail.com>
2023-05-15 13:36:42 +0200

    Added SK version, updated script by CR

--- js/f__tooltip.js
+++ js/f__tooltip.js
@@ -1,29 +1,39 @@
-const pointerElements = document.querySelectorAll('.lpFeatures__pointer')
-const tooltipElement = document.querySelector('.lpFeatures__tooltip')
+const run_tooltip = () => {
+  const pointerElements = document.querySelectorAll('.lpFeatures__pointer')
+  const tooltipElement = document.querySelector('.lpFeatures__tooltip')
+  let hovered = false
+  
+  pointerElements.forEach((element) => {
+    element.addEventListener('mouseenter', function(e) {
+      const add_content = () => {
+        if (e.target.closest('.lpFeatures__pointer') !== element) {
+          return
+        }
 
-pointerElements.forEach((element) => {
-  element.addEventListener('mouseenter', function(e) {
-    if (e.target.closest('.lpFeatures__pointer') !== element) {
-      return;
-    }
+        const parentItem = element.parentElement
+        const dataItem = parentItem.dataset.item
+    
+        const targetElement = document.querySelector('.' + dataItem)
+        if (targetElement) {
+          const clonedElement = targetElement.cloneNode(true)
+          tooltipElement.innerHTML = ''
+          tooltipElement.appendChild(clonedElement)
+        }
+    
+        tooltipElement.style.display = 'block'
+        tooltipElement.style.top = e.clientY + 'px'
+        tooltipElement.style.left = e.clientX + 'px'
+        hovered = true
+      }
 
-    const parentItem = element.parentNode
-    const dataItem = parentItem.dataset.item
-
-    const targetElement = document.querySelector('.' + dataItem)
-    if (targetElement) {
-      const clonedElement = targetElement.cloneNode(true)
+      hovered === true ? 0 : add_content()
+    })
+  
+    element.addEventListener('mouseleave', function() {
+      tooltipElement.style.display = 'none'
       tooltipElement.innerHTML = ''
-      tooltipElement.appendChild(clonedElement)
-    }
-
-    tooltipElement.style.display = 'block'
-    tooltipElement.style.top = e.clientY + 'px'
-    tooltipElement.style.left = e.clientX + 'px'
+      hovered = false
+    })
   })
-
-  element.addEventListener('mouseleave', function() {
-    tooltipElement.style.display = 'none'
-    tooltipElement.innerHTML = ''
-  })
-})
+}
+run_tooltip()
